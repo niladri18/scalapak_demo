607f739187f71bbf0755d809237b179dc622cea0
Niladri Gomes <niladri9@login18.chn>
2023-07-26 09:32:20 -0700

    set blocking as  a variable in run.py

--- run.py
+++ run.py
@@ -4,9 +4,13 @@ import time
 
 n = 5
 output = [0]*n
+Mb = 32
+Nb = 32
+print(f"Blocking: {Mb}x{Nb}")
+
 for i in range(1,n+1):
     t1 = time.time()
-    output[i-1] = subprocess.run(["mpirun", "-n", str(2**(i+1)), "./scaling", "1024", "1024","1024","32","32"],\
+    output[i-1] = subprocess.run(["mpirun", "-n", str(2**(i+1)), "./scaling", "1024", "1024","1024",str(Mb),str(Nb)],\
                         stdout = subprocess.PIPE, universal_newlines = True).stdout
     t2 = time.time()
     print(f"Nproc: {2**(i+1)} Finished in {(t2-t1)*1000} millisec")
